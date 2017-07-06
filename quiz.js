4746d364d01caf3a454f8a4f8655f63403338891
Andree Peyrot <Andree@users-imac-8.attlocal.net>
2017-07-06 13:26:27 -0400

    first commit

---
+++ quiz.js
@@ -0,0 +1,133 @@
+// var pointsCount = 0;
+//
+// function gradeQuiz (){
+//   var correctAnswer = documentByElementid
+// }
+// var correctAnswer = 2;
+// var pointsCount = ("correctAnswer");
+//  function correctAnswer(button_id ="correctAnswer") {
+//   //  if button_id = ("Indianapolis" + id="Olivia" +
+//   //  id="4th" + id="Mike Pence" + id="Cardinal");
+// }
+// var pointsCount = 0;
+//
+// function gradeQuiz (){
+//   var correctAnswer = doc
+// }
+var pointsCount = 0;
+let correctPoints = document.getElementById('correctAnswer');
+
+ function correctAnswer(){
+   for(var i = 0; i <= x.length; i++){
+     if(x[i] == " "){
+     correctPoints++;
+     }
+     if( x[i] == "." || x[i] == "?" || x[i] == "!"){
+       correctPoints++;
+     console.log(pointsCount)
+    }
+    correctAnswer();
+  }
+}
+
+ document.getElementById('correctPoints').innerHTML = "You have " + pointsCount + " points. ";
+
+
+
+
+
+// function reg() {
+//   window.captureEvents(Event.SUBMIT);
+//   window.onsubmit = hit;
+// }
+//
+// function hit() {
+//   console.log('hit');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class QuickyQuiz
+//   constant let button type //added constant for ease of read
+//     let button type "button1="b1"// button ids, sub class needed for  id and button type
+//     let button type "button2="b2"
+//     let button type "button3="b3"
+//     let button type "button4="b4";
+// if (var1 = 0) {
+//   return true;
+// }
+// if id="q1" button type "b3" =  2;
+//
+//     console.log ("b3");
+//
+//
+// if id="q2" button type "b2" = 2;
+//
+//     console.log ("b2");
+//
+//
+//
+//  if id="q3" button type "b3"  = 2;
+//     console.log ("b3");
+//
+//
+//  if id="q4" button type "b3" = 2;
+//     console.log ("b3");
+//
+//
+//         alert
+//  if id="q5" button type "button3" = 2;
+//  if id="q5" button type "button1, button3,button4" = - 1;
+// console.log ("b1" + "b2"+ "b3" +"b4")
+//       alert
+//       console.log result ("q1" + "q2" + "q3" + "q4" + "q5");
+//             alert
+// //
+// <p id="q5"></p>
+// <script>
+// document.getElementById("q5").innerHTML = ("button1 +button3 + button4");
+// document.getElementById("q5").innerHTML = ("button2" + 2);
+// </script>
+// <script>
+// <p id="q4"></p>
+// document.getElementById("q4").innerHTML = ("button1 +button2 + button4");
+// document.getElementById("q4").innerHTML = ("button3" + 2);
+// </script>
+// <p id="q3"></p>
+// <script>
+// document.getElementById("q3").innerHTML = ("button1 +button3 + button2");
+// document.getElementById("q3").innerHTML = ("button4" + 2);
+// </script>
+// <p id="q2"></p>
+// <script>
+// document.getElementById("q2").innerHTML = ("button1 +button3 + button4");
+// document.getElementById("q2").innerHTML = ("button2" + 2);
+// </script>
+// <p id="q1"></p>
+// <script>
+// document.getElementById("q1").innerHTML = ("button1 +button2 + button4");
+// document.getElementById("q1").innerHTML = ("button3" + 2);
+// </script>
+//
+// document.getElementById("q1").innerHTML = "";
+// document.getElementById("q2").innerHTML = "";
+// document.getElementById("q3").innerHTML = "";
+// document.getElementById("q4").innerHTML = "";
+// document.getElementById("q5").innerHTML = "";
